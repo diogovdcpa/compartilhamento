ca4536283158a33b327c7988aad5d02add1fc74c
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-26 17:42:27 -0300

    fatiando layout

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>Copyright &copy; 2020 <a href="#">Sharing APP</a>.</strong>
+    Todos os direitos reservados.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.0.5
+        <b>Versão</b> 1.0.0
     </div>
 </footer>
