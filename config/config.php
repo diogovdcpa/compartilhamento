1bc92b171eb6be36d561137ce28d540d910985d2
Eng.Diogo <dev.diogovdcpa@gmail.com>
2020-11-26 17:33:30 -0300

    up code

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+define("SERVIDOR", "localhost");
+define("BANCO", "portoseguro_paginacao");
+define("USUARIO", "root");
+define("SENHA", "");
+
+
+define('CONTROLLER_PADRAO', 'home');
+define('METODO_PADRAO', 'index');
+define('NAMESPACE_CONTROLLER', 'app\\controllers\\');
+
+define('URL_BASE', 'http://localhost/compartilhamento');
+define('ETC', 'http://localhost/compartilhamento/etc/');
